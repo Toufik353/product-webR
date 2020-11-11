8dfd8336b326f725b3535229c2cb4db968a02b17
Toufik353 <toufik.desai12@gmail.com>
2020-11-12 00:03:02 +0530

    updated

--- src/productPage/productPage.js
+++ src/productPage/productPage.js
@@ -5,7 +5,12 @@ import classes from "./productPage.module.css"
 class productPage extends Component {
 state={
     currentRow:-1,
-    productData:localStorage.getItem("appData")===null?[]:JSON.parse(localStorage.getItem("appData")).productsPage.products
+    productData:localStorage.getItem("appData")===null?[]:JSON.parse(localStorage.getItem("appData")).productsPage.products,
+ clickedArr:[] ,
+ isChecked:true,
+ CurrentId:-1,
+           
+
 }
 
 //  data=localStorage.getItem("appData")===null?[]:JSON.parse(localStorage.getItem("appData"));
@@ -26,17 +31,173 @@ state={
 
 
 // }
-handleDelet = (rowId)=>{
+// handleDelet = (rowId)=>{
+//     this.setState({currentRow:rowId})
+//     // let productD = this.state.ProductList
+//     let filterdata = this.state.productData.filter((item,index)=>rowId !== index)
+//     let acctdata = JSON.parse(localStorage.getItem('appData'));
+//     acctdata['productsPage']['products'] = filterdata
+//     localStorage.setItem('appData', JSON.stringify(acctdata));
+//   this.setState({
+//     productData:filterdata
+//   })
+// }
+
+
+handleDelet=(rowId)=>{
     this.setState({currentRow:rowId})
-    // let productD = this.state.ProductList
-    let filterdata = this.state.productData.filter((item,index)=>index !== rowId)
+   var filterdata= this.state.productData.filter((item,index)=>{
+       return((this.state.currentRow!==index))
+       
+    })
     let acctdata = JSON.parse(localStorage.getItem('appData'));
-    acctdata['productsPage']['products'] = filterdata
-    localStorage.setItem('appData', JSON.stringify(acctdata));
-  this.setState({
-    productData:filterdata
-  })
+       acctdata['productsPage']['products'] = filterdata
+       localStorage.setItem('appData', JSON.stringify(acctdata));
+       this.setState({
+            productData:filterdata
+          })
 }
+
+
+// handleCheckedBox=(sId)=>{
+//     //   this.setState({clickedArr:this.clickedArr.push(sId)});
+//     let clicked=this.state.clickedArr;
+//     clicked.push(sId);
+//     console.log(clicked)
+//     this.setState({clickedArr:clicked});
+//           let newArr=localStorage.getItem("checkList")===null?[]:JSON.parse(localStorage.getItem("checkList"));
+
+  
+//     let newlocal=newArr.map(item=>{
+//         if(item.id===sId){
+//             this.setState({isChecked:!this.state.isChecked})
+//             item.checked=this.state.isChecked;
+//         }
+//         else{
+//             var clickedData={
+//                         id:sId,
+//                         checked:this.state.isChecked
+//                     }
+//                     newArr.push(clickedData)
+//                     localStorage.setItem("checkList",JSON.stringify(newArr))
+//         }
+//     })
+    //------------------------------
+    // handleCheckedBox=(sId)=>{
+    //     // alert("Bookmark clicked")
+    //     let clicked=this.state.clickedArr;
+    //     clicked.push(sId);
+    //     console.log(clicked)
+    //     this.setState({clickedArr:clicked});
+
+    //     var VideoArr=localStorage.getItem("checkList")===null?[]:JSON.parse(localStorage.getItem("checkList"));
+    //     var data={
+    //       id:sId,
+    //       isChecked:false
+    //     }
+    //     var isExist=false;
+
+    //     const updatedArr= VideoArr.map(item=>{
+    //       // return( 
+    //         if(item.id===sId){
+    //         item.isChecked=!item.isChecked;
+    //         isExist=true;
+    //          this.setState({isChecked:item.isChecked})
+
+    //        }
+    //        return item;
+    //      })
+    //      if(!isExist){
+    //       updatedArr.push(data)
+    //       this.setState({isChecked:false})
+
+    //      }
+    //     // var updatedArr=[data]
+    //     localStorage.setItem("checkList",JSON.stringify(updatedArr))
+    //   }
+
+    //   handleDeletAll=()=>{
+    //       var datafromLoc=JSON.parse(localStorage.getItem("checkList"))
+    //      let nArr=[];
+    //      nArr= datafromLoc.map((item,index)=>{
+    //              if(item.isChecked===true){
+    //                  item.isChecked=false;
+    //                  return item.id;
+    //              }
+                 
+    //       })
+    //     //   console.log(nArr);
+
+    //        let deleteSelectedItems= this.state.productData.filter((item,index)=>{
+    //          return !nArr.includes(index); 
+
+
+    //         })
+    //         // console.log(nArr.slice(-1,nArr.length))
+    //         for(var j=nArr.length;j>=1;j--){
+    //             nArr.pop()
+    //         }
+    //         console.log(nArr)
+    //         console.log(deleteSelectedItems);
+
+    //         this.setState({productData:deleteSelectedItems})
+    //         datafromLoc=[];
+    //         localStorage.setItem("checkList",JSON.stringify(datafromLoc))
+
+    //   }
+    
+//     var clickedData={
+//         id:sId,
+//         checked:this.state.isChecked
+//     }
+// newArr.push(clickedData) 
+//    localStorage.setItem("checkList",JSON.stringify(newArr))
+        // if(i===sId){[0,1]
+        //   clicked=this.state.clickedArr.filter((item,index)=>{
+        //      return sId!==index
+        //   })
+        //   this.setState({clickedArr:clicked})
+        // }
+        // else{
+        //     clicked.push(sId);
+        //     console.log(clicked)
+        //     this.setState({clickedArr:clicked})
+        // }
+    
+    //   this.clickedArr.filter()
+        // console.log(this.state.clickedArr)
+    
+    
+
+
+
+// }
+// class Checkbox extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         isChecked: true,
+//       };
+//     }
+//     toggleChange = () => {
+//       this.setState({
+//         isChecked: !this.state.isChecked,
+//       });
+//     }
+//     render() {
+//       return (
+//         <label>
+//           <input type="checkbox"
+//             checked={this.state.isChecked}
+//             onChange={this.toggleChange}
+//           />
+//           Check Me!
+//         </label>
+//       );
+//     }
+//   }
+
+
 
     render() {
     //   const  data=localStorage.getItem("appData")===null?[]:JSON.parse(localStorage.getItem("appData"));
@@ -62,7 +223,7 @@ console.log(this.state.productData)
                        {this.state.productData.map((item,index)=>{
             return(
                <tr key={index}>
-               <td><input type="checkbox"/></td>
+               <td><input onClick={()=>{this.handleCheckedBox(index)}} type="checkbox" checked={false} /></td>
                <td>{item.category}</td>
                <td>{item.unitSold}</td>
                <td>{item.stock}</td>
@@ -75,6 +236,7 @@ console.log(this.state.productData)
                    
                         
                     </table>
+                    <button onClick={this.handleDeletAll}>Delet selected items</button>
                 </div>
 
                 <div className={classes.rightSection}>
